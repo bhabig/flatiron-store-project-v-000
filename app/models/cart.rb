4132b3c0f8348e4c4f5f5351954768963be48057
Bob Habig <rfhab2@gmail.com>
2017-01-24 23:58:39 -0500

    tried to add a before_create check for unplaced orders (via unsubmitted cart status). made cart model specs fail bc they didn't dummy up users the way its needed. nbd. code works, tests just didn't expect it.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,6 +5,22 @@ class Cart < ActiveRecord::Base
 
   validates_uniqueness_of :id
 
+=begin
+  #probably a good idea in a real use case, but may not be a good idea here. all of my actual feature tests for cart passed with this code, but it doesn't work for spec/model/cart_spec.rb bc the tests don't give it a user to find and use.
+
+  before_create :check_for_unsubmitted_carts #**** method needs to return true or false - true will allow save
+
+  def check_for_unsubmitted_carts #assuming this works, update cart_creation to use the cart with false status.
+    u = User.find_by(id: self.user_id)
+    cart_to_make_user_use = u.carts.find{|c| c.status == false } unless u.carts.empty? || !u
+    if cart_to_make_user_use
+      return false
+    else
+      return true
+    end
+  end
+=end
+
   def total
     self.line_items.map do |line_item|
       item = Item.find(line_item.item_id)
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,5 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :carts
   has_many :items, through: :carts
-
-  before_initialize :check_for_open_orders
-
-  def check_for_open_orders
-    binding.pry
-  end
+  
 end
