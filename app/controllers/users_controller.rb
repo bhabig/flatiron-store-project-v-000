6f333b3ab9320dc34a8b74d231b4a419135a61da
Jennifer Pazos <jmp521@gmail.com>
2017-01-25 22:24:16 -0600

    removed unnecessary code from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,5 @@
 class UsersController < ApplicationController
 
 
-  def new
 
-  end
-
-  def create
-  end
-
-  def destroy
-
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:password, :email, :name, :current_cart_id)
-  end
 end
